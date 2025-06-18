afb8df5bbb27695b9f6cc516af0295a36d35bcd2
Gus Jacobs <gusjacobs006@gmail.com>
2025-06-18 16:13:23 -0400

    Update urls.py

--- pegumax_project/bot_monitor/urls.py
+++ pegumax_project/bot_monitor/urls.py
@@ -8,7 +8,7 @@ app_name = 'bot_monitor' # This line is crucial for namespacing
 
 
 urlpatterns = [
-    path('receive-logs/', LogReceiverView.as_view(), name='receive_logs'),
+    path('receive-logs/', views.receive_logs_view, name='receive_logs'),
     path('dashboard-data/', DashboardDataView.as_view(), name='dashboard_data'),
     path('get-logs/', BotLogDataView.as_view(), name='get_bot_logs'),
     path('acknowledge-logs/', AcknowledgeLogsView.as_view(), name='acknowledge_logs'),
